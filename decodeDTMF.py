4d50a6c473d9f2227dfb2e6f7c1bcdd158b80a61
BrunaKimura <brunamk@al.insper.edu.br>
2017-09-20 16:58:29 -0300

    gera gráfico a cada 1 segundo

--- decodeDTMF.py
+++ decodeDTMF.py
@@ -1,38 +1,70 @@
 import sounddevice as sd
 import matplotlib.pyplot as plt
 import numpy as np
+from matplotlib import animation 
 
 fs = 44100
-duration = 3
+duration = 1
 
-audio = sd.rec(int(duration*fs), fs, channels=1)
-sd.wait()
+# audio = sd.rec(int(duration*fs), fs, channels=1)
+# sd.wait()
 
-y = audio[:,0]
+# y = audio[:,0]
 
-
-t=3
-tempo=np.linspace(0, t, fs*t)
-# omega1=(2 * np.pi *f1)
-# omega2=(2 * np.pi *f2)
-
-# y1 = np.sin(tempo*omega1)
-# y2 = np.sin(tempo*omega2)
-
-# y=y1+y2
+# t=1
+#     tempo=np.linspace(0, t, fs*t)
 
 
+# t=1
+# tempo=np.linspace(0, t, fs*t)
 
-x = tempo
-print (len(x), len(y))
-plt.plot(x, y)
-plt.xlim(0,0.15)
-plt.xlabel('tempo')
-plt.ylabel('onda')
+# while True:
+#     audio = sd.rec(int(duration*fs), fs, channels=1)
+#     sd.wait()
+
+#     y = audio[:,0]
+
+
+    
+#     x = tempo
+
+#     plt.close()
+#     plt.plot(x, y)
+#     plt.xlim(0,0.015)
+#     plt.xlabel('tempo')
+#     plt.ylabel('onda')
+#     plt.show(block=False)
+
+fig = plt.figure()
+ax1 = fig.add_subplot(1,1,1)
+plt.xlabel('Tempo')
+plt.ylabel('Sinal')
+
+def animate(i):
+    t=1
+    tempo=np.linspace(0, t, fs*t)
+    x = tempo
+
+    audio = sd.rec(int(duration*fs), fs, channels=1)
+    sd.wait()
+
+    y = audio[:,0]
+
+    ax1.clear()
+    ax1.plot(x[0:1000], y[0:1000])
+    
+    # line.set_data(x, y)
+    # return lin,
+
+anim = animation.FuncAnimation(fig, animate, interval=1000)
+    
+
 plt.show()
 
-#reproduz o som
-sd.play(y, fs)
 
-#aguarda fim da reprodução
-sd.wait()
+
+# #reproduz o som
+# sd.play(y, fs)
+
+# #aguarda fim da reprodução
+# sd.wait()
